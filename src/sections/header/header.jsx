20e1dafa1cdec214afbc3b14b6c82878913fabf4
Zoor <hectorllorca7@gmail.com>
2022-04-27 23:31:08 +0200

    change font, fix links

--- src/sections/header/header.jsx
+++ src/sections/header/header.jsx
@@ -6,23 +6,21 @@ const Header = ({ theme, switchTheme }) => {
   return (
     <div className="header container">
       <div className="logo">
-        <img src={zoro} alt="zoro" />
+        <img src={zoro} alt="zoro " />
       </div>
       <div className="navbar">
         <ul>
           <li>
-            <a href="https://zoor7.github.io/portfolio3/#Skills">Skills</a>
+            <a href="#Skills">Skills</a>
           </li>
           <li>
-            <a href="https://zoor7.github.io/portfolio3/#Experience">
-              Experience
-            </a>
+            <a href="#Experience">Experience</a>
           </li>
           <li>
-            <a href="https://zoor7.github.io/portfolio3/#Projects">Projects</a>
+            <a href="#Projects">Projects</a>
           </li>
           <li>
-            <a href="https://zoor7.github.io/portfolio3/#Contact">Contact</a>
+            <a href="#Contact">Contact</a>
           </li>
         </ul>
       </div>
